6401b307f70f6c1496a29adb9023593ae20a9fb4
prateek-2005 <0000psyp@gmail.com>
2026-03-10 12:10:35 +0530

    [Prateek] Add. added feature-UC7-Addition-With-Target-Unit-Specification

---
+++ src/main/java/com/quantity/QuantityMeasurementApp/Length.java
@@ -0,0 +1,45 @@
+package com.quantity.QuantityMeasurementApp;
+
+public class Length {
+
+    private final double value;
+    private final LengthUnit unit;
+
+    public Length(double value, LengthUnit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public double toBaseUnit() {
+        return unit.toBaseUnit(value);
+    }
+
+    public double convertTo(LengthUnit targetUnit) {
+
+        double inchValue = this.toBaseUnit();
+        return targetUnit.fromBaseUnit(inchValue);
+    }
+    public double add(Length other, LengthUnit targetUnit) {
+
+        double thisInch = this.toBaseUnit();
+        double otherInch = other.toBaseUnit();
+
+        double totalInch = thisInch + otherInch;
+
+        return targetUnit.fromBaseUnit(totalInch);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Length other = (Length) obj;
+
+        return Double.compare(this.toBaseUnit(), other.toBaseUnit()) == 0;
+    }
+}
